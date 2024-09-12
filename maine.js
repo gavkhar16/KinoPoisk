048dc5eab0ddfa0f8f98f62243279c48c854132d
gavkhar16 <khabibullaevagavkhar@gmail.com>
2024-09-13 01:47:02 +0500

    did it

--- maine.js
+++ maine.js
@@ -1,35 +1,99 @@
-let changeThemeBtn = document.querySelector(".themeChage")
+let changeThemeBtn = document.querySelector(".themeChange")
 let body = document.querySelector("body")
 
-changeThemeBtn.addEventListener("click", themeThema)
+changeThemeBtn.addEventListener("click", changeTheme)
 
-if (localStorage.getItem("theme") == "dark"){
-    changeThemeBtn.classList.add("darkTheme")
-    body.classList.add("dark")
+if (localStorage.getItem("theme") === "dark") {
+    changeThemeBtn.classList.add("darkTheme");
+    body.classList.add("dark");
+}
+function changeTheme() {
+    if (localStorage.getItem("theme") === "dark") {
+      changeThemeBtn.classList.toggle('darkTheme');
+      body.classList.toggle("dark");
+      localStorage.setItem("theme", "white");
+    } else {
+      changeThemeBtn.classList.toggle('darkTheme');
+      body.classList.toggle("dark");
+      localStorage.setItem("theme", "dark");
+    }
+  }
+  
+  let searchBtn = document.querySelector(".search button");
+  searchBtn.addEventListener("click", searchMovie);
+  
+  let loader = document.querySelector('.loader');
+  
+  document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      searchMovie();
+    }
+  });
+
+async function searchMovie() {
+    loader.style.display = "block";
+
+    let searchText = document.getElementById('dd').value;
+    console.log(searchText);
+
+    let response = await sendRequest("http://www.omdbapi.com/", "GET", {
+        "apikey": "74920bef",
+        "t": searchText
+    });
+
+    if (response.Response == "False") {
+        loader.style.display = "none";
+        alert(response.Error);
+        console.log(response);
+    } else {
+        let main = document.querySelector(".main");
+        main.style.display = "block";
+
+        let movieTitle = document.querySelector('.movieTitle h2');
+        movieTitle.innerHTML = response.Title;
+
+        let movieImg = document.querySelector(".movieImg");
+        movieImg.style.backgroundImage = `url(${response.Poster})`;
+
+        let detailsList = ["Language", "Actors", "Country", "Genre", "Released", "Runtime", "imdbRating"];
+        let movieInfo = document.querySelector(".movieInfo");
+        movieInfo.innerHTML = "";
+
+        for (let i = 0; i < detailsList.length; i++) {
+            let param = detailsList[i];
+            let desc = `<div class="desc darckBg">
+                <div class="title">${param}</div>
+                <div class="value">${response[param]}</div>
+            </div>`;
+            movieInfo.innerHTML += desc;
+        }
+
+        loader.style.display = "none";
+    }
 }
 
-function themeThema(){
-    if(localStorage.getItem("theme") == "dark"){
-        changeThemeBtn.classList.remove("darkTheme")
-        body.classList.remove("dark")
-        localStorage.setItem("theme", "white")
-    }
-    else{
-        changeThemeBtn.classList.add("darkTheme")
-        body.classList.add("dark")
-        localStorage.setItem("theme", "dark")  
-    }
-    // changeThemeBtn.classList.toggle('darkTheme')
-    // body.classList.toggle("dark")
-}
-let searchBtn = document.querySelector(".search button")
-searchBtn.addEventListener("click", searchMovie)
 
-let loader = document.querySelector('.loader')
-document.addEventListener('keydown')
-function searchMovie() {
-    loader.style.diplay = "blok"
+async function sendRequest(url, method, data) {
+    if (method == "POST") {
+        let response = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
 
-    let searchText = document.querySelector(".search input").value
-    console.log(searchText)
+        response = JSON.parse(response)
+        return response
+    } else if (method == "GET") {
+        url = url + "?" + new URLSearchParams(data)
+        let response = await fetch(url, {
+            method: "GET"
+        })
+
+        response = await response.json()
+        return response
+    }
 }
